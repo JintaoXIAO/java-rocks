20e44df7e8d583fb25a6012ef7181b179b708f97
jintao_xiao <xiaojintao@outlook.com>
2020-12-21 22:01:23 +0800

    update

---
+++ testng/src/main/java/testng/demo/Calc.java
@@ -0,0 +1,15 @@
+package testng.demo;
+
+public class Calc {
+  int add(int a, int b) {
+    return a + b;
+  }
+
+  int sub(int a, int b) {
+    return a - b;
+  }
+
+  int mul(int a, int b) {
+    return a * b;
+  }
+}

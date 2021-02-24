c086b01c4d701aa0cd46cc7d9119cd26b6c4b720
jintao_xiao <jintao.xiao@sap.com>
2021-02-24 19:36:45 +0800

    leetcode :: p15 :: not finished

---
+++ leetcode/src/test/java/p15/$3SumTest.java
@@ -0,0 +1,20 @@
+package p15;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class $3SumTest {
+
+  @DataProvider(name = "default")
+  public Object[][] dataset() {
+    return new Object[][]{
+
+    };
+  }
+
+  @Test
+  public void testThreeSum() {
+  }
+}

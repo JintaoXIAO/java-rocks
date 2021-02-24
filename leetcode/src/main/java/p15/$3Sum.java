249eda20b4286cff6fcc26e3d5e369083d2108ad
jintao_xiao <jintao.xiao@sap.com>
2021-02-24 19:36:45 +0800

    leetcode :: p15

--- leetcode/src/main/java/p15/$3Sum.java
+++ leetcode/src/main/java/p15/$3Sum.java
@@ -1,50 +1,44 @@
 package p15;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class $3Sum {
 
   public static void main(String[] args) {
     $3Sum s = new $3Sum();
-    List<List<Integer>> r = s.threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+    List<List<Integer>> r = s.threeSum(new int[]{-1, -1, 0, 1, 2, -1, -4});
     System.out.println(r);
   }
 
   public List<List<Integer>> threeSum(int[] nums) {
-    if (nums.length < 3) return Collections.emptyList();
-    Arrays.sort(nums);
     List<List<Integer>> result = new ArrayList<>();
-    calculate(nums, 0, nums.length - 1, result);
+    if (nums.length < 3) return result;
+    Arrays.sort(nums);
+
+    for (int i = 0; i < nums.length; i++) {
+      int cur = nums[i];
+      if (cur > 0) return result;
+      if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+      int l = i + 1, r = nums.length - 1;
+      while (l < r) {
+        int tmp = cur + nums[l] + nums[r];
+        if (tmp == 0) {
+          result.add(List.of(cur, nums[l], nums[r]));
+
+          while (l < r && nums[l] == nums[l + 1]) l++;
+          while (l < r && nums[r] == nums[r - 1]) r--;
+
+          l++;
+          r--;
+
+        } else if (tmp < 0) {
+          l++;
+        } else {
+          r--;
+        }
+      }
+    }
     return result;
-  }
-
-  void calculate(int[] nums, int l, int r, List<List<Integer>> result) {
-    if (r - l < 2) return;
-
-    if (r - l == 2 && (nums[l] + nums[l + 1] + nums[l + 2] == 0)) {
-      result.add(List.of(nums[l], nums[l + 1], nums[l + 2]));
-    }
-
-    int sum;
-    sum = nums[l] + nums[l + 1] + nums[r];
-    if (sum > 0) calculate(nums, l, r - 1, result);
-    else if (sum == 0) result.add(List.of(nums[l], nums[l+1], nums[r]));
-    for (int i = r - 1; i > l; i--) {
-      sum = nums[l] + nums[r] + nums[i];
-      if (sum == 0) List.of(nums[l], nums[i], nums[r]);
-      else if (sum>0) break;
-    }
-
-    sum = nums[l] + nums[r - 1] + nums[r];
-    if (sum < 0) calculate(nums, l + 1, r, result);
-    else if (sum == 0) result.add(List.of(nums[l], nums[r-1], nums[r]));
-    for (int i = l + 1; i < r; i++) {
-      sum = nums[l] + nums[r] + nums[i];
-      if (sum == 0) List.of(nums[l], nums[i], nums[r]);
-      else if (sum < 0) break;
-    }
   }
 }

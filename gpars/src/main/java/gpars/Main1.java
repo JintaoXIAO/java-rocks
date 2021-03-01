b3b7bb2035d8e6346a6914f6281aed914ada53f8
jintao_xiao <jintao.xiao@sap.com>
2021-03-01 17:20:29 +0800

    update

--- gpars/src/main/java/gpars/Main1.java
+++
@@ -1,7 +0,0 @@
-package gpars;
-
-public class Main1 {
-  public static void main(String[] args) {
-    System.out.println("gpars rocks!");
-  }
-}
--- kafka/src/main/java/kafka/demo/Main2.java
+++
@@ -1,6 +0,0 @@
-package kafka.demo;
-
-public class Main2 {
-  public static void main(String[] args) {
-  }
-}
